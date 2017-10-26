eb6a0c328ede9259910f8cc7a20ffcf20a15ccd2
aishwarya-majumdar <33061742+aishwarya-majumdar@users.noreply.github.com>
2017-10-26 16:28:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,76 @@
-const TelegramBot = require('node-telegram-bot-api');
- 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = '453397017:AAH3ypq1z6OscGuRlQt_RzYE9g9_o9v3aZE';
- 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const apiaiApp = require('apiai')("7f15354fd8784fefa82137532014d187");
- 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-	console.log("message : ", msg);
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
- 
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
- 
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
+var pageAccessToken = 'EAAXkcs07pZB4BAFqdaqFjAsUZBPC3dIBVw4tDTzNZB4R0PoNsZAR2biVQQ9rtGLz5b5KnZB1Syw2WtXrj8cX5s0qvAbX7gkf2pIjs872O0WUpZBtZAUZA0YuZAmRNt1xPOePZCMLcYSO65PUFqKvyyYDl0bW9yPon5EZAY6ZCbSw2w7RPAZDZD';
+
+const server = app.listen(process.env.PORT || 6001, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
- 
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  
-	let sender = msg.chat.id;
-	let text = msg.text;
-	console.log("sender = " + sender);
-	console.log("text = " + text);
 
-	var options = {    
-		proxyHost: '10.31.8.34',
-		proxyPort: 8080,
-		sessionId: '1234'
-	};
+app.get('/webhook', (req, res) => {
+  if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'pwc_bot') {
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    res.status(403).end();
+  }
+});
 
-  let apiai = apiaiApp.textRequest(text, options);
-  
-  //console.log("aitext : ", aiText);
+/* Handling all messenges */
+app.post('/webhook', (req, res) => {
+  console.log(req.body);
+  if (req.body.object === 'page') {
+    req.body.entry.forEach((entry) => {
+      entry.messaging.forEach((event) => {
+        if (event.message && event.message.text) {
+          sendMessage(event);
+        }
+      });
+    });
+    res.status(200).end();
+  }
+});
+
+const request = require('request');
+const apiaiApp = require('apiai')("7f15354fd8784fefa82137532014d187");
+
+function sendMessage(event) {
+  let sender = event.sender.id;
+  let text = event.message.text;
+console.log("sender = " + sender);
+console.log("text = " + text);
+  let apiai = apiaiApp.textRequest(text, {
+    sessionId: '1234' // use any arbitrary id
+  });
 
   apiai.on('response', (response) => {
   
    	console.log("response = " + response);
   let aiText = response.result.fulfillment.speech;
-	
-	// send a message to the chat acknowledging receipt of their message
-	bot.sendMessage(chatId, aiText);
-    
+
+    request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token: pageAccessToken},
+      method: 'POST',
+      json: {
+        recipient: {id: sender},
+        message: {text: aiText}
+      }
+    }, (error, response) => {
+      if (error) {
+          console.log('Error sending message: ', error);
+      } else if (response.body.error) {
+          console.log('Error: ', response.body.error);
+      }
+    });
 
   });
-  
+
   apiai.on('error', (error) => {
     console.log(error);
   });
 
   apiai.end();
-});
+}
